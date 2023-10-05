5e8190eaed445b85c9998a1e084a850f707e1375
serkanmercn <serkanmercan7@gmail.com>
2023-10-05 11:00:06 +0300

    TaximeterCalculator
    TaximeterCalculator

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int km;
+        double perKm = 2.20, total = 10;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Mesafeyi KM cinsinden giriniz: ");
+        km = input.nextInt();
+
+        total += (km * perKm);
+        total = (total < 20) ? 20 : total;
+        System.out.println("Toplam Tutar: " + total);
+    }
+}
